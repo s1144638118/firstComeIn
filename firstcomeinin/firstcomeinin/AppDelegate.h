c2b9c7a9316dac79bd3bb18347b7c4dca8fd9eb9
lanou3g <1144638118@qq.com>
2015-07-09 09:42:41 +0800

    first添加新工程

---
+++ firstcomeinin/firstcomeinin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstcomeinin
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 岩松@灬D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstcomeinin/firstcomeinin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstcomeinin
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 岩松@灬D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
